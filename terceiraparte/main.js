459cfba628c00503912144db06556c983a723438
Souza <gss020955000@gmail.com>
2024-07-08 17:20:21 -0300

    adicionando mais um codigo ao repositorio

---
+++ terceiraparte/main.js
@@ -0,0 +1,3 @@
+var nome = window.prompt('Qual seu nome?') //faz uma pergunta e pede a resposta do usuario
+
+window.alert('É um grande prazer te conhcecer, ' + nome + '!') // sinal de mais serve para concatenação
