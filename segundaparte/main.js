3d934d803c9355e2c238b6f4a46b8b07d88dd3b3
Souza <gss020955000@gmail.com>
2024-07-08 17:10:41 -0300

    adicionando mais codigos

---
+++ segundaparte/main.js
@@ -0,0 +1,3 @@
+window.alert('Bem vindo') //mostra uma mensagem quando o navegador é atualizado
+window.confirm('Confirma o acesso?') // insere uma janela com ok ou cancelar
+window.prompt('Qual seu nome?') //faz uma pergunta e pede a resposta do usuario
